51d8ad25af9e159901057369427a30fa59258f8e
Yago Tobio <201802168@alu.comillas.edu>
2023-11-29 16:25:41 +0100

    Commit + ML Model

--- ProyectoFinalAsignatura/spaceapp.py
+++ ProyectoFinalAsignatura/spaceapp.py
@@ -1,17 +1,20 @@
 from dash import Dash, html, dcc, Input, Output, State
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from io import BytesIO
 from collections import Counter
+from io import BytesIO
+import dash_leaflet as dl
 
 
 import base64
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Dataframe
 df = pd.read_csv("assets/astronauts.csv")
+df_space_missions = pd.read_csv("assets/space_missions.csv", encoding="ISO-8859-1")
+df_space_missions_failure = pd.read_csv("assets/Space_Corrected.csv", encoding="ISO-8859-1")
 
 # * Data pre-processing
 df = df[df["Year"].notna()]
@@ -26,45 +29,134 @@ df["Year Interval"] = df["Year Interval"].astype(str)
 df["State"] = df["Birth Place"].str.split(",").str[-1].str.strip()
 
 # Define lists of majors you consider 'Typical'
-typical_majors_keywords = ['Engineering', 'Science', 'Physics', 'Mathematics', 'Chemistry', 'Biology', 'Astronomy', 'Aeronautics']
+typical_majors_keywords = [
+    "Engineering",
+    "Science",
+    "Physics",
+    "Mathematics",
+    "Chemistry",
+    "Biology",
+    "Astronomy",
+    "Aeronautics",
+]
+
 
 # Function to categorize majors based on keywords
 def categorize_major(major):
     major = str(major)  # Ensure the major is a string
     for keyword in typical_majors_keywords:
         if keyword in major:
-            return 'Typical'
-    return 'Wacky/Unusual'
+            return "Typical"
+    return "Wacky/Unusual"
+
 
 # Apply this function to the 'Undergraduate Major' column
-df['Major Category'] = df['Undergraduate Major'].apply(categorize_major)
+df["Major Category"] = df["Undergraduate Major"].apply(categorize_major)
 
 # Count the number of astronauts in each categorized major
-major_counts = df.groupby(['Major Category', 'Undergraduate Major']).size().reset_index(name='Number of Astronauts')
+major_counts = (
+    df.groupby(["Major Category", "Undergraduate Major"])
+    .size()
+    .reset_index(name="Number of Astronauts")
+)
 
 # Count the number of astronauts per state
 state_counts = df["State"].value_counts().reset_index()
 state_counts.columns = ["State", "Astronaut Count"]
 
-#! - Word Count Bubble for the Space Missions 
+#! - Word Count Bubble for the Space Missions
 # Split the 'Missions' column into individual missions and count them
-mission_list = df['Missions'].dropna().str.split(', ').sum()
+mission_list = df["Missions"].dropna().str.split(", ").sum()
 mission_counts = Counter(mission_list)
 
 # Generate the word cloud from frequencies
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(mission_counts)
+wordcloud = WordCloud(
+    width=800, height=400, background_color="white"
+).generate_from_frequencies(mission_counts)
 
 # Convert the word cloud image to a string of base64 to display in Dash
 plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
 img = BytesIO()
-plt.savefig(img, format='png', bbox_inches='tight', pad_inches=0)
+plt.savefig(img, format="png", bbox_inches="tight", pad_inches=0)
 img.seek(0)
 wordcloud_string = base64.b64encode(img.getvalue()).decode()
 
-wordcloud_image = html.Img(src='data:image/png;base64,{}'.format(wordcloud_string), style={'width': '100%', 'height': 'auto'})
+wordcloud_image = html.Img(
+    src="data:image/png;base64,{}".format(wordcloud_string),
+    style={"width": "100%", "height": "auto"},
+)
 
+
+"""
+DATA PRE-PROCESSING FOR THE SPACE MISSIONS:
+======================================================
+"""
+# * Data pre-processing for the space missions:
+# Convert 'Date' to datetime and extract the year
+df_space_missions["Date"] = pd.to_datetime(df_space_missions["Date"])
+df_space_missions["Year"] = df_space_missions["Date"].dt.year
+# Group by year and count the number of missions
+missions_per_year = (
+    df_space_missions.groupby("Year").size().reset_index(name="Number of Missions")
+)
+
+# Extract country name from 'Location'
+df_space_missions["Country"] = (
+    df_space_missions["Location"].str.split(",").str[-1].str.strip()
+)
+# Count the number of missions per country
+missions_per_country = df_space_missions["Country"].value_counts().reset_index()
+missions_per_country.columns = ["Country", "Number of Missions"]
+
+# Group by company and mission status
+grouped_df = (
+    df_space_missions.groupby(["Company", "MissionStatus"])
+    .size()
+    .reset_index(name="Count")
+)
+# Calculate the total missions for each company
+total_missions_per_company = (
+    grouped_df.groupby("Company")["Count"].sum().reset_index(name="TotalMissions")
+)
+# Merge to get total missions alongside status counts
+grouped_df = pd.merge(grouped_df, total_missions_per_company, on="Company")
+# Calculate total missions
+total_missions = grouped_df["Count"].sum()
+# Calculate the percentage for each company
+grouped_df["Percentage"] = grouped_df["Count"] / total_missions * 100
+
+
+# Create the choropleth map
+fig_missions_map = px.choropleth(
+    missions_per_country,
+    locations="Country",
+    locationmode="country names",
+    color="Number of Missions",
+    title="Number of Space Missions by Country",
+)
+
+fig_sunburst = px.sunburst(
+    grouped_df,
+    path=["Company", "MissionStatus"],
+    values="Count",
+    title="Space Missions: Companies and Mission Success Rates",
+    custom_data=["Percentage"],
+)
+# Update hovertemplate to show percentages
+fig_sunburst.update_traces(
+    hovertemplate="<b>%{label}</b><br>Count: %{value}<br>Percentage: %{customdata[0]:.2f}%"
+)
+
+fig_3d = px.scatter_3d(
+    df_space_missions,
+    x="Year",
+    y="MissionStatus",  # You might need to map this to a numerical scale
+    z="Price",  # Same as above
+    color="Company",  # Could be another categorical variable
+    title="3D Scatter Plot of Space Missions",
+)
 
 """
 FIGURES
@@ -86,17 +178,19 @@ fig_choropleth = px.choropleth(
 fig_choropleth.update_layout(geo=dict(bgcolor="rgba(0,0,0,0)"))
 
 fig_education = px.scatter(
-    major_counts, 
-    x='Undergraduate Major', 
-    y='Major Category', 
-    size='Number of Astronauts',
-    color='Major Category', 
-    title='Astronauts by Major: Typical vs. Wacky/Unusual'
+    major_counts,
+    x="Undergraduate Major",
+    y="Major Category",
+    size="Number of Astronauts",
+    color="Major Category",
+    title="Astronauts by Major: Typical vs. Wacky/Unusual",
 )
 
 # Improve readability
 fig_education.update_layout(xaxis_tickangle=-45)
-fig_education.update_traces(marker=dict(opacity=0.7))  # Adjust opacity for better visualization
+fig_education.update_traces(
+    marker=dict(opacity=0.7)
+)  # Adjust opacity for better visualization
 
 app = Dash(
     __name__, external_stylesheets=[dbc.themes.SUPERHERO, dbc.icons.FONT_AWESOME]
@@ -158,6 +252,53 @@ learn_card = dbc.Card(
     className="mt-4",
 )
 
+missions_card = dbc.Card(
+    dbc.CardBody(
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id="missions-map", figure=fig_missions_map), width=6),
+                dbc.Col(
+                    [
+                        dcc.Dropdown(
+                            id="mission-status-dropdown",
+                            options=[
+                                {"label": status, "value": status}
+                                for status in df_space_missions[
+                                    "MissionStatus"
+                                ].unique()
+                            ],
+                            value="Success",  # Default value
+                            clearable=False,
+                        ),
+                        dcc.Graph(id="missions-time-series"),
+                    ],
+                    width=6,
+                ),
+                dbc.Col(
+                    dcc.Graph(id="nested-pie-chart", figure=fig_sunburst),
+                    width=6,
+                ),
+                dbc.Col(
+                    [
+                        dcc.Dropdown(
+                            id="company-filter",
+                            options=[
+                                {"label": company, "value": company}
+                                for company in df_space_missions["Company"].unique()
+                            ],
+                            value=None,
+                            multi=True,
+                            placeholder="Select Company",
+                        ),
+                        # Other filters as needed...
+                        dcc.Graph(id="3d-scatter-plot"),
+                    ],width=6,
+                ),
+            ]
+        )
+    )
+)
+
 astronaut_card = dbc.Card(
     dbc.CardBody(
         dbc.Row(
@@ -182,7 +323,6 @@ astronaut_card = dbc.Card(
                             value=df["Gender"].unique().tolist(),
                             inline=True,
                         ),
-                        
                     ],
                     width=2,
                 ),  # Adjust width as needed
@@ -194,12 +334,18 @@ astronaut_card = dbc.Card(
                             min=df["Year"].min(),
                             max=df["Year"].max(),
                             value=[df["Year"].min(), df["Year"].max()],
-                            marks={str(year): str(year) for year in range(df['Year'].min(), df['Year'].max() + 1) if year % 5 == 0},
+                            marks={
+                                str(year): str(year)
+                                for year in range(
+                                    df["Year"].min(), df["Year"].max() + 1
+                                )
+                                if year % 5 == 0
+                            },
                             allowCross=False,
                         ),
                         dcc.Graph(id="bar-chart"),
                         dcc.Graph(id="us-map", figure=fig_choropleth),
-                        dcc.Graph(id='major-bubble-chart', figure=fig_education),
+                        dcc.Graph(id="major-bubble-chart", figure=fig_education),
                         html.H3("Word Soup de las misiones espaciales"),
                         wordcloud_image,
                     ],
@@ -212,6 +358,11 @@ astronaut_card = dbc.Card(
     className="mt-4",
 )
 
+failure_explanation_card = dbc.Card(
+    dbc.CardBody(
+
+    )
+)
 
 """
 COMPONENTS
@@ -227,6 +378,7 @@ tabs = dbc.Tabs(
             label="Astronauts",
             className="pb-4",
         ),
+        dbc.Tab(missions_card, tab_id="tab-3", label="Missions", className="pb-4"),
     ],
     id="tabs",
     active_tab="tab-1",
@@ -264,7 +416,11 @@ CALLBACKS
 
 
 @app.callback(
-    [Output("bar-chart", "figure"), Output("us-map", "figure"), Output("major-bubble-chart", "figure")],
+    [
+        Output("bar-chart", "figure"),
+        Output("us-map", "figure"),
+        Output("major-bubble-chart", "figure"),
+    ],
     [
         Input("year-range-slider", "value"),
         Input("status-selector", "value"),
@@ -272,12 +428,13 @@ CALLBACKS
     ],
 )
 def update_visualizations(selected_year_range, selected_status, selected_gender):
-    
-    filtered_df = df[(df['Year'] >= selected_year_range[0]) & (df['Year'] <= selected_year_range[1])]
+    filtered_df = df[
+        (df["Year"] >= selected_year_range[0]) & (df["Year"] <= selected_year_range[1])
+    ]
     if selected_status:
-        filtered_df = filtered_df[filtered_df['Status'].isin(selected_status)]
+        filtered_df = filtered_df[filtered_df["Status"].isin(selected_status)]
     if selected_gender:
-        filtered_df = filtered_df[filtered_df['Gender'].isin(selected_gender)]
+        filtered_df = filtered_df[filtered_df["Gender"].isin(selected_gender)]
 
     grouped_df = (
         filtered_df.groupby(["Year Interval", "Status"])
@@ -308,18 +465,20 @@ def update_visualizations(selected_year_range, selected_status, selected_gender)
     map_fig.update_layout(geo=dict(bgcolor="rgba(0,0,0,0)"))
 
     # Prepare data for the bubble chart
-    major_counts = filtered_df['Undergraduate Major'].value_counts().reset_index()
-    major_counts.columns = ['Undergraduate Major', 'Number of Astronauts']
-    major_counts['Major Category'] = major_counts['Undergraduate Major'].apply(categorize_major)
+    major_counts = filtered_df["Undergraduate Major"].value_counts().reset_index()
+    major_counts.columns = ["Undergraduate Major", "Number of Astronauts"]
+    major_counts["Major Category"] = major_counts["Undergraduate Major"].apply(
+        categorize_major
+    )
 
     # Create the bubble chart
     bubble_fig = px.scatter(
         major_counts,
-        x='Undergraduate Major',
-        y='Major Category',
-        size='Number of Astronauts',
-        color='Major Category',
-        title='Astronauts by Major: Typical vs. Wacky/Unusual'
+        x="Undergraduate Major",
+        y="Major Category",
+        size="Number of Astronauts",
+        color="Major Category",
+        title="Astronauts by Major: Typical vs. Wacky/Unusual",
     )
 
     # Adjust layout for the bubble chart
@@ -328,6 +487,68 @@ def update_visualizations(selected_year_range, selected_status, selected_gender)
 
     # Return all the figures
     return bar_fig, map_fig, bubble_fig
+
+
+@app.callback(
+    Output("missions-time-series", "figure"),
+    [Input("mission-status-dropdown", "value")],
+)
+def update_mission_time_series(selected_status):
+    filtered_df = df_space_missions[
+        df_space_missions["MissionStatus"] == selected_status
+    ]
+    missions_per_year = (
+        filtered_df.groupby("Year").size().reset_index(name="Number of Missions")
+    )
+
+    fig = px.line(
+        missions_per_year,
+        x="Year",
+        y="Number of Missions",
+        title="Number of Space Missions Over Time",
+        markers=True,
+    )
+
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(count=5, label="5y", step="year", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            rangeslider=dict(visible=True),
+            type="date",
+        )
+    )
+    # Add range slider configuration here...
+
+    return fig
+
+
+@app.callback(
+    Output("3d-scatter-plot", "figure"),
+    [Input("company-filter", "value")],  # Add other inputs as needed
+)
+def update_3d_scatter(selected_companies):
+    filtered_df = df_space_missions
+    if selected_companies:
+        filtered_df = filtered_df[df_space_missions["Company"].isin(selected_companies)]
+
+    # Update 3D scatter plot with filtered data
+    fig = px.scatter_3d(
+        filtered_df,
+        x="Year",
+        y="MissionStatus",  # Adjust mapping to numerical scale if needed
+        z="Price",  # Same as above
+        color="Company",  # Or another categorical variable
+        title="3D Scatter Plot of Space Missions",
+    )
+    return fig
 
 
 if __name__ == "__main__":
